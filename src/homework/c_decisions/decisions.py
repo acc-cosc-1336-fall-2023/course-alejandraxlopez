dcf85e94e78b1c9b5ce0e3f29026d12f1571df25
alejandraxlopez <143046331+alejandraxlopez@users.noreply.github.com>
2023-09-17 04:19:10 +0000

    Creating a function that returns the rating for a faculty member! (RATIO)

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,28 @@
-#
+
+def get_options_ratio (option, total):
+    ratio = option / total
+    return ratio
+
+def get_faculty_rating(ratio):
+    rating = ""
+    if (ratio >= 0.9 and ratio < 1.0):
+        rating = "Excellent"
+    elif(ratio >= 0.8 and ratio < 0.9):
+        rating = "Very Good"
+    elif(ratio >= 0.7 and ratio < 0.8):
+        rating = "Good"
+    elif(ratio >= 0.6 and ratio < 0.7):
+        rating = "Needs Improvement"
+    elif(ratio >= 0 and ratio < 0.59):
+        rating = "Unacceptable"
+    else:
+        rating = "Invalid Rating"
+
+    return rating
+    
+
+
+
+
+ 
+    
