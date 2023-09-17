d8501586383055a2197693f3cf55b1893c402e6d
alejandraxlopez <143046331+alejandraxlopez@users.noreply.github.com>
2023-09-17 04:30:36 +0000

    * greater than or equal to correction *

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -5,7 +5,7 @@ def get_options_ratio (option, total):
 
 def get_faculty_rating(ratio):
     rating = ""
-    if (ratio >= 0.9 and ratio < 1.0):
+    if (ratio >= 0.9 and ratio <= 1.0):
         rating = "Excellent"
     elif(ratio >= 0.8 and ratio < 0.9):
         rating = "Very Good"
@@ -13,8 +13,9 @@ def get_faculty_rating(ratio):
         rating = "Good"
     elif(ratio >= 0.6 and ratio < 0.7):
         rating = "Needs Improvement"
-    elif(ratio >= 0 and ratio < 0.59):
+    elif(ratio >= 0 and ratio <= 0.59):
         rating = "Unacceptable"
+
     else:
         rating = "Invalid Rating"
 
