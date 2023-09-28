ec8d756f693d0f3a8d7004af73353083ac84d051
alejandraxlopez <143046331+alejandraxlopez@users.noreply.github.com>
2023-09-28 21:29:36 +0000

    DISPLAYING TIME, USING MODULUS OPERATOR, EPOCH SECONDS

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,11 @@
-#
+
+import value_return
+
+hour = value_return.get_hour(3800)
+minutes = value_return.get_minutes(3800)
+seconds = value_return.get_seconds(3800)
+
+time = (f"{hour:02d}:{minutes:02d}:{seconds:02d}")
+# using an f-string makes concatenation easier to read & format
+# 02d displays hour, mins, & seconds with two digits
+print(time)
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,17 @@
-#
+
+def get_hour(epoch_seconds):
+    hour = epoch_seconds // 3600
+    return hour
+# double slash performs floor division, rounding result to nearest integer
+def get_minutes(epoch_seconds):
+   minutes = epoch_seconds // 60 % 60
+   return minutes
+
+def get_seconds(epoch_seconds):
+   seconds = epoch_seconds % 3600 % 60
+   return seconds
+# test case ex: 3800s / 3600s will have R200s...R200s / 60s will have R20s
+
+    
+    
+    
